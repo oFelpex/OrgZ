0bc853fb70cc861e815618007b381316fdb7a9a0
Felipe Costa <felipe95176@gmail.com>
2025-06-01 16:34:30 -0300

    💄 Change primary color for light theme

--- src/themes/lightTheme.tsx
+++ src/themes/lightTheme.tsx
@@ -4,7 +4,7 @@ const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#1976d2", // azul padrão
+      main: "#fcfcfd",
     },
     secondary: {
       main: "#dc004e", // rosa padrão
