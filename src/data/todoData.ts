b3861a12786a085027192191c34e94373bef3335
Felipe Costa <felipe95176@gmail.com>
2025-06-05 11:05:48 -0300

    🎨 Create a file for todo data

---
+++ src/data/todoData.ts
@@ -0,0 +1,7 @@
+import type { TodoData } from "../types/todoTyoes";
+
+export const todoData: TodoData = {
+  todo: ["Estudar React", "Fazer exercícios"],
+  onGoing: ["Aprendendo MUI"],
+  done: ["Configurar projeto"],
+};
